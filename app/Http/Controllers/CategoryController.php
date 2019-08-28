a2bdb1ee325ec199d995da2058ac562e246a471c
Sushan Paudyal <sushan.paudyal@gmail.com>
2019-08-28 23:36:33 +0545

    Update Category

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -67,7 +67,8 @@ class CategoryController extends Controller
      */
     public function update(Request $request, Category $category)
     {
-        //
+        $category->update(['name' => $request->name, 'slug' => str_slug($request->name)]);
+        return response('Updated', Response::HTTP_ACCEPTED);
     }
 
     /**
